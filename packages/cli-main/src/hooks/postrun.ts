268df849911ee9f9a62ca12d2c279822a8682a4d
Ariel Caplan <ariel.caplan@shopify.com>
2022-07-01 00:33:23 +0300

    Log command start and finish

--- packages/cli-main/src/hooks/postrun.ts
+++ packages/cli-main/src/hooks/postrun.ts
@@ -1,7 +1,9 @@
-import {analytics} from '@shopify/cli-kit'
+import {analytics, output} from '@shopify/cli-kit'
 import {Hook} from '@oclif/core'
 
 // This hook is called after each successful command run. More info: https://oclif.io/docs/hooks
-export const hook: Hook.Postrun = async (_options) => {
+export const hook: Hook.Postrun = async (options) => {
   await analytics.reportEvent()
+  const command = options.Command.id.replace(/:/g, ' ')
+  output.debug(`Completed command ${command}`)
 }
