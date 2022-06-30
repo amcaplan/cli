4f40eeeb8be2b8c8b0ba1a71e698eb497c75060a
Ariel Caplan <ariel.caplan@shopify.com>
2022-07-01 01:06:31 +0300

    Allow accessing all logs directly from the shopify logs command

--- packages/cli-main/src/cli/commands/logs.ts
+++ packages/cli-main/src/cli/commands/logs.ts
@@ -1,10 +1,22 @@
-import {Command} from '@oclif/core'
-import {output} from '@shopify/cli-kit'
+import {Command, Flags} from '@oclif/core'
+import {cli, output} from '@shopify/cli-kit'
 
 export default class Logs extends Command {
   static description = 'View full debug logs from the Shopify CLI'
 
+  static flags = {
+    ...cli.globalFlags,
+    stream: Flags.string({
+      hidden: false,
+      description: 'Choose which logs to view',
+      options: ['cli', 'create-app', 'create-hydrogen'],
+      default: 'cli',
+      env: 'SHOPIFY_FLAG_STREAM',
+    }),
+  }
+
   async run(): Promise<void> {
-    await output.pageLogs()
+    const {flags} = await this.parse(Logs)
+    await output.pageLogs(flags.stream)
   }
 }
